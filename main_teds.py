9ce4964432cfffb9ca4e49f8f4bac2898386c597
the-grandson <lcsmneto@gmail.com>
2022-07-01 21:03:39 +0100

    Reformulate classes

--- main_teds.py
+++ main_teds.py
@@ -13,8 +13,9 @@
 
 from PyQt5 import QtWidgets, QtCore
 import sys
+import datetime
 from teds_editor import Ui_editorMainWindow
-from  teds_data_model import Meta_TEDS_Data_Block, Meta_TEDS_Data_Block, TransducerChannel_TEDS_Data_Block
+from  teds_data_model import TEDS_ACCESS_CODES, Meta_TEDS_Data_Block, Meta_TEDS_Data_Block, TransducerChannel_TEDS_Data_Block
 import teds_utils
 
 # Create meta teds model
@@ -29,104 +30,93 @@ class ApplicationWindow(QtWidgets.QMainWindow):
 
         self.ui = Ui_editorMainWindow()
         self.ui.setupUi(self)
-        self.loadTedsMetaTable()
-        self.loadChannelTeds()
+        #self.loadTedsMetaTable()
+        #self.loadChannelTeds()
         # Register handle for Generate UUID btn
         self.ui.pushButton.clicked.connect(self.generateUUID)
         # Register handle for Save .bin action
         self.ui.actionSave_bin.triggered.connect(self.saveBin)
-
-    def loadTedsMetaTable(self):
+        # Init the MetaTEDS table
         self.ui.metaTedsTable.setColumnWidth(0, 400)
         self.ui.metaTedsTable.setColumnWidth(1, 200)
-        self.ui.metaTedsTable.setRowCount(9)
-        # Set field description
-        # To-Do change this function to have the same logic as loadChannelTeds
-        self.ui.metaTedsTable.setItem(0, 0, QtWidgets.QTableWidgetItem(Meta_TEDS_Data_Block.UUID[2]))
-        self.ui.metaTedsTable.setItem(1, 0, QtWidgets.QTableWidgetItem(Meta_TEDS_Data_Block.OholdOff[2]))
-        self.ui.metaTedsTable.setItem(2, 0, QtWidgets.QTableWidgetItem(Meta_TEDS_Data_Block.SHoldOff[2]))
-        self.ui.metaTedsTable.setItem(3, 0, QtWidgets.QTableWidgetItem(Meta_TEDS_Data_Block.TestTime[2]))
-        self.ui.metaTedsTable.setItem(4, 0, QtWidgets.QTableWidgetItem(Meta_TEDS_Data_Block.MaxChan[2]))
-        self.ui.metaTedsTable.setItem(5, 0, QtWidgets.QTableWidgetItem(Meta_TEDS_Data_Block.CGroup[2]))
-        self.ui.metaTedsTable.setItem(6, 0, QtWidgets.QTableWidgetItem(Meta_TEDS_Data_Block.VGroup[2]))
-        self.ui.metaTedsTable.setItem(7, 0, QtWidgets.QTableWidgetItem(Meta_TEDS_Data_Block.GeoLoc[2]))
-        self.ui.metaTedsTable.setItem(8, 0, QtWidgets.QTableWidgetItem(Meta_TEDS_Data_Block.Proxies[2]))
-
-        for i in range(self.ui.metaTedsTable.rowCount()):
-            item = self.ui.metaTedsTable.item(i, 0)
-            # Make this item not editable
-            item.setFlags(item.flags() ^ QtCore.Qt.ItemIsEditable)
-            blankItem = QtWidgets.QTableWidgetItem()
-            blankItem.setData(QtCore.Qt.EditRole, 0)
-            self.ui.metaTedsTable.setItem(i, 1, blankItem)
-            # Callback function to catch item changes
-        self.ui.metaTedsTable.itemChanged.connect(metaTedsTableChange)
-
-    def loadChannelTeds(self):
+        self.load_teds_data_block(self.ui.metaTedsTable, meta_teds, metaTedsTableChange, None)
+        # Init the ChannelTEDS table
         self.ui.transducerChannelTable.setColumnWidth(0, 400)
-        self.ui.transducerChannelTable.setColumnWidth(1, 200)
-        self.ui.transducerChannelTable.setRowCount(len(channel_teds.fields))
-        # Set field description
-        for i in range(0,len(channel_teds.fields)):
-            field = channel_teds.fields[i]
+        self.ui.transducerChannelTable.setColumnWidth(1, 300)
+        self.load_teds_data_block(self.ui.transducerChannelTable, channel_teds, channelTedsTableChange, channelTedsComboBoxChanged)
+
+    def load_teds_data_block(self, qtable, teds_data_block, table_edit_callback, combobox_edit_callback):
+        # Set the table number of rows
+        qtable.setRowCount(len(teds_data_block.fields))
+        # Iterate all fields of the teds data block
+        for i in range(0,len(teds_data_block.fields)):
+            field = teds_data_block.fields[i]
             if field.enum :
                 # If the TEDS field domain is an enumeration, use a combobox
                 # Use the enumeration to fill the combobox with values
                 combo = QtWidgets.QComboBox()                
-                combo.currentTextChanged.connect(lambda value, index=i: channelTedsComboBoxChanged(value, index))
+                combo.currentTextChanged.connect(lambda value, index=i: combobox_edit_callback(value, index))
                 for value in field.enum:
                     combo.addItem(value.name)
-                self.ui.transducerChannelTable.setCellWidget(i,1,combo)
+                qtable.setCellWidget(i,1,combo)
             else:
                 # Else, assume the field value is an integer
                 blankItem = QtWidgets.QTableWidgetItem()
-                blankItem.setData(QtCore.Qt.EditRole, 0)
-                self.ui.transducerChannelTable.setItem(i, 1, blankItem)
+                if i==0:
+                    # Fisrt item is the teds identification field
+                    # Make it not editable
+                    blankItem.setData(QtCore.Qt.EditRole, TEDS_ACCESS_CODES(field.teds_class).name)
+                    blankItem.setFlags(blankItem.flags() ^ QtCore.Qt.ItemIsEditable)
+                else:
+                    blankItem.setData(QtCore.Qt.EditRole, field.get_value())
+                qtable.setItem(i, 1, blankItem)
             # Use the field description to set the description cell of the table
             item = QtWidgets.QTableWidgetItem(field.get_description())
             # Make the description not editable
             item.setFlags(item.flags() ^ QtCore.Qt.ItemIsEditable)
-            self.ui.transducerChannelTable.setItem(i, 0, item)
+            qtable.setItem(i, 0, item)
 
-        # Callback function to catch item changes
-        self.ui.transducerChannelTable.itemChanged.connect(metaTedsTableChange)
+        # Callback function to catch changes in the table items
+        qtable.itemChanged.connect(table_edit_callback)
 
     def generateUUID(self):
         # Data model generate new uuid
         meta_teds.set_uuid(teds_utils.generate_uuid())
         # Get table uuid cell
-        uuid_cell = self.ui.metaTedsTable.item(0, 1)
+        uuid_cell = self.ui.metaTedsTable.item(1, 1)
         # Display uuid as string, int not working
-        uuid_cell.setData(QtCore.Qt.DisplayRole, meta_teds.uuid_field.get_value().hex())
+        print(meta_teds.uuid_field.get_value())
+        uuid_cell.setData(QtCore.Qt.DisplayRole, meta_teds.uuid_field.get_value().hex('-'))
 
     def saveBin(self):
         # Name the file with uuid from meta teds
-        metaTedsFile = open(meta_teds.uuid_field.get_value().hex()+".bin", "wb")
-        metaTedsFile.write(meta_teds.to_bytes())
+        tab_index = self.ui.metaTedsTab_2.currentIndex()
+        if  tab_index == 0:
+            metaTedsFile = open(meta_teds.uuid_field.get_value().hex('-')+".bin", "wb")
+            metaTedsFile.write(meta_teds.to_bytes())
+        elif tab_index == 1:
+            channelTedsFile = open("channel_teds_"+datetime.datetime.today().strftime('%Y-%m-%d_%H-%M-%S')+".bin", "wb")
+            channelTedsFile.write(channel_teds.to_bytes())
 
 def metaTedsTableChange(item):
-    # TO-DO, implement remaining items
-    if item.row() > 0 and item.row() < 5:
-        print("Item row: {}, item column: {}, item value: {}\n".format(item.row(), item.column(), item.data(QtCore.Qt.DisplayRole)))
-        if item.row() == 1:
-            meta_teds.set_oholdoff(item.data(QtCore.Qt.DisplayRole))
-        elif item.row() == 2:
-            meta_teds.set_sholdoff(item.data(QtCore.Qt.DisplayRole))
-        elif item.row() == 3:
-            meta_teds.set_test_time(item.data(QtCore.Qt.DisplayRole))
-        elif item.row() == 4:
-            meta_teds.set_max_chan(item.data(QtCore.Qt.DisplayRole))
+    # The item (cell) row is the same index of the TEDS field
+    if item.row() != 1:
+        # ignore UUID field changes
+        meta_teds.fields[item.row()].set_value(item.data(QtCore.Qt.DisplayRole))
 
+# Callback function for the channel TEDS comboboxes
 def channelTedsComboBoxChanged(value, index):
-    #print(value)
-    #print(index)
+    # Get the new value from the enum that defines the TEDS field domain
     new_value = channel_teds.fields[index].enum[value].value
+    # Set the new value in the TEDS field
     channel_teds.fields[index].set_value(new_value)
-    print(channel_teds.fields[index].value)
+    # print(channel_teds.fields[index].value)
 
-
+# Callback function for the Channel TEDS table
 def channelTedsTableChange(item):
-    print(item.row())
+    # The item (cell) row is the same index of the TEDS field
+    channel_teds.fields[item.row()].set_value(item.data(QtCore.Qt.DisplayRole))
+    # print(channel_teds.fields[item.row()].value)
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
--- teds_utils.py
+++ teds_utils.py
@@ -13,6 +13,8 @@
 
 import uuid
 from sys import getsizeof
+import numpy as np
+from numpy import float32, uint16, int16, uint32, int32, uint8, int8, frombuffer
 
 # Type 4, UUID, Globally Unique Identifier UUID, size 10
 # TO-DO, implement as in the standard definition
@@ -24,3 +26,218 @@ def calc_length(data_block):
 
 def calc_checksum(self):
     pass
+
+# Utility functions to convert data
+to_uint8 = lambda value : uint8(value)
+to_int8 = lambda value : int8(value)
+to_uint16 = lambda value : uint16(value)
+to_int16 = lambda value : int16(value)
+to_uint32 = lambda value : uint32(value)
+to_int32 = lambda value : int32(value)
+to_float32 = lambda value : float32(value)
+# list_to_uint8 = lambda flist : np.array(flist,dtype="uint8").tobytes()
+# list_to_int8 = lambda flist : np.array(flist,dtype="int8").tobytes()
+# list_to_uint16 = lambda flist : np.array(flist,dtype="uint16").tobytes()
+# list_to_int16 = lambda flist : np.array(flist,dtype="int16").tobytes()
+# list_to_uint32 = lambda flist : np.array(flist,dtype="uint32").tobytes()
+# list_to_int32 = lambda flist : np.array(flist,dtype="int32").tobytes()
+list_to_type = lambda vtype, vlist : np.array(vlist,dtype=vtype).tobytes()
+# Infer what conversion function is most appropriate to a TEDS Field
+def infer_conversion_function(teds_field):
+    dtype_octets = 0
+    dtype_conversion = None
+
+    if teds_field.data_type == uint8:
+        dtype_octets = 1
+        dtype_conversion = to_uint8
+    elif teds_field.data_type == int8:
+        dtype_octets = 1
+        dtype_conversion = to_int8
+    elif teds_field.data_type == uint16:
+        dtype_octets = 2
+        dtype_conversion = to_uint16
+    elif teds_field.data_type == int16:
+        dtype_octets = 2
+        dtype_conversion = to_int16
+    elif teds_field.data_type == uint32:
+        dtype_octets = 4
+        dtype_conversion = to_uint32
+    elif teds_field.data_type == int32:
+        dtype_octets = 4
+        dtype_conversion = to_int32
+    elif teds_field.data_type == float32:
+        dtype_octets = 4
+        dtype_conversion = to_float32
+
+    if dtype_octets < teds_field.get_value_length():
+        # If the data type octets are less than the field value octets
+        # Then assume the value is a list with N values
+        # N = field_octets/dtype_octets (TO-DO)
+        teds_field.value_to_data_type = lambda vlist, vtype=teds_field.data_type.dtype : np.array(vlist,dtype=vtype).tobytes()
+    else:
+        teds_field.value_to_data_type = dtype_conversion
+
+
+# Number of octets in Type, Length of a TEDS TLV field
+TL_OCTETS = 2
+
+# Utility classes
+
+class TEDS_Block:
+
+    def __init__(self, teds_data_block):
+        self.teds_data_block = teds_data_block
+        self.length = calc_length(self.value)
+        self.checksum = calc_checksum(self.teds_data_block)
+
+# Utility class that holds functions and attributes common to all TEDS data blocks
+class TEDS_Data_Block():
+
+    def __init__(self):
+        # List to hold references to all teds fields
+        self.fields = []
+
+    def to_bytes(self):
+        barray = bytearray(b'')
+        for field in self.fields:
+            try:
+                barray.append(field.get_TLV().get_bytes())
+            except:
+                barray.extend(field.get_TLV().get_bytes())
+        return barray
+
+    # This method will only load a TEDS with the fields defined in the constructor
+    # A Meta TEDS will all fields will fail to load
+    def load_from_bytearray(self, bytearr):
+        # bytearr must be 'bytearray object
+        assert isinstance(bytearr, bytearray)
+
+        # Current position in bytearray
+        seek = 0
+        for field in self.fields:
+            # print("Loading array section: {} - {}".format(seek,seek + field.get_total_length()))
+            # print("Section size: {}".format(len(bytearr[seek:seek + field.get_total_length()])))
+            # print(bytearr[seek:seek + field.get_total_length()])
+            field.load_bytes(bytearr[seek:seek + field.get_total_length()])
+            seek += field.get_total_length()
+
+class TEDS_Field():
+
+    def __init__(self, type, name, description, data_type, n_octets):
+        self.type = uint8(type)
+        self.name = name
+        self.description = description
+        self.data_type = data_type
+        self.length = uint8(n_octets)
+        self.value = None
+        self.tlv = None
+        self.enum = None
+        self.optional = False
+        self.value_to_data_type = None
+
+    def get_type(self):
+        return self.type
+
+    def get_name(self):
+        return self.name
+
+    def get_description(self):
+        return self.description
+
+    def get_data_type(self):
+        return self.data_type
+
+    def get_value_length(self):
+        # Fixed length of 4 octects for this structure
+        return self.length
+
+    def get_total_length(self):
+        # Fixed length of 4 octects for this structure
+        return self.get_value_length()+TL_OCTETS
+
+    def set_value_enum(self,enum):
+        # If the value is based in an enumeration, keep a reference
+        self.enum = enum
+
+    def set_conversion_function(self, function):
+        self.value_to_data_type = function
+
+    def is_optional(self):
+        self.optional = True
+
+    # Use the provided function to convert the value o the correct teds field data type
+    # If the function fails, try to cast it
+    # If given value has the correct type, do not apply conversion
+    def set_value(self, value):
+        print(type(value))
+        print([self.data_type, bytes, bytearray])
+        if type(value) not in [self.data_type, bytes, bytearray]:
+            try:
+                # If a conversion function is not known, try to define it
+                if self.value_to_data_type is None:
+                    infer_conversion_function(self)
+                # Use the generic conversion function
+                self.value = self.value_to_data_type(value)
+            except:
+                # In last resort, try to cast the value
+                self.value = type(self.data_type)(value)
+        else:
+            self.value = value
+
+    def get_value(self):
+        return self.value
+
+    def get_TLV(self):
+        if self.tlv == None:
+            self.tlv = TEDS_TLV_Block(self)
+        return self.tlv
+
+    def get_bytes(self):
+        return self.get_TLV.get_bytes()
+
+    def load_bytes(self, barray):
+        try:
+            assert type(barray) is bytearray
+            assert len(barray) == self.get_total_length()
+            assert barray[0] == self.type
+            assert barray[1] == self.length
+            # First two elements of the buffer are type and 
+            if type(self.data_type) == bytes:
+                self.set_value(barray[TL_OCTETS:self.get_total_length()])
+            else:
+                self.set_value(frombuffer(barray[TL_OCTETS:self.get_total_length()], dtype=self.data_type.dtype))
+        except:
+            print("TEDS Field: {}, loaded bytes don't match.".format(self.name))
+            #raise ValueError("TEDS Field: {}, loaded bytes don't match.".format(self.name))
+
+# All TEDS prepared by a transducer manufacturer use a Type/Length/Value (TLV) data structure.
+# The “Type“ field is a 1 octet tag that identifies the TLV, similar in function to HTML or XML tags. 
+# The “Length” field specifies the number of octets of the value field.
+# The “Value” field is the actual data.
+class TEDS_TLV_Block():
+
+    def __init__(self, teds_field):
+
+        self.type = teds_field.get_type()
+        self.length = teds_field.get_value_length()
+        self.teds_field = teds_field
+
+    #TO-DO correct this method
+    def get_bytes(self):
+        barr = bytearray(b'')
+        try:
+            barr.extend(self.type)
+        except:
+            barr.append(self.type)
+        try:
+            barr.extend(self.length)
+        except:
+            barr.append(self.length)
+        try:
+            barr.extend(self.teds_field.get_value())
+        except:
+            barr.append(self.teds_field.get_value())
+        if len(barr) != self.length + TL_OCTETS:
+            raise ValueError("TEDS field type: {}, expected encoding length is: {}, should be: {}"
+                .format(self.type, len(barr)-TL_OCTETS, self.length))
+        return barr
